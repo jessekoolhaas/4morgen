4fab30f6f4d8f8b988df68ea2401889a6f8305ef
jesse koolhaas <jessekoolhaas@hotmail.com>
2016-09-03 13:09:40 -0700

    -

--- app/angular.js
+++ app/angular.js
@@ -10,11 +10,23 @@ var myapp = angular.module('myapp',
      ]
      );
 
+     myapp.run(function ($rootScope) {
+       var api = "https://api-test.4morgen.org/";
+
+        //  Inlog
+         $rootScope.auth                    = api + "v1/authentication";
+         $rootScope.authLogin               = api + "v1/authentication/login";
+         $rootScope.authLogout              = api + "v1/authentication/logoff";
+
+        //  Winkels
+         $rootScope.winkelCategorie         = api + "v1/categories";
+         $rootScope.winkelSubCategorie      = api + "v1/category/{categoryId}/subcategories";
+         $rootScope.winkelItems             = api + "v1/category/{categoryId}/subcategories/{subcategoryIds}/items?orderBy={orderBy}&skip={skip}&top={top}&includeCount={includeCount}";
+         $rootScope.winkelToevoegenFav      = api + "v1/currentuser/favoriteshops";
+         $rootScope.winkelRedirect          = api + "v1/tracking/redirect/{winkelId}";
+
+         $rootScope.winkelRelated           = api + "v1/shop/{shopId}/related";
+         $rootScope.winkelDisplay           = api + "v1/shop/{shopId}";
 
 
-
-
-
-  myapp.controller('databaseCtl', function ($scope,$stateParams, MyHttpInterceptor ){
-    $scope.currentCategoryName = $stateParams.Category;
-  });
+     });
