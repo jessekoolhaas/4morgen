169c8d57a0d309f4479e548eb38e0a3c1c026a16
jesse koolhaas <jessekoolhaas@hotmail.com>
2016-09-03 13:56:35 -0700

    -

--- site/js/controllers/dashboardCtl.js
+++ site/js/controllers/dashboardCtl.js
@@ -1,4 +1,4 @@
-myapp.controller('dashboardCtl', ['$scope', '$http','$location','$window','$state','$rootScope' function ($scope, $http, $location,$window,$state,$rootScope) {
+myapp.controller('dashboardCtl', ['$scope', '$http','$location','$window','$state','$rootScope', function ($scope, $http, $location,$window,$state,$rootScope) {
 
 
   $scope.$state = $state;
