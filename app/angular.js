83faf597114d9a5c128c771af92f750e4a8c7683
jesse koolhaas <jessekoolhaas@hotmail.com>
2016-09-09 11:29:16 -0700

    Stond nog op test-api

--- app/angular.js
+++ app/angular.js
@@ -11,7 +11,7 @@ var myapp = angular.module('myapp',
      );
 
      myapp.run(function ($rootScope) {
-       var api = "https://api-test.4morgen.org/";
+       var api = "https://api.4morgen.org/";
 
          // Inlog
          $rootScope.auth                    = api + "v1/authentication";
@@ -40,7 +40,7 @@ var myapp = angular.module('myapp',
 
          // dashboard
          $rootScope.donatieJaar             = api + "v1/donations/currentuser/yearly";
-         $rootScope.donatieLijst            = api + "https://api.4morgen.org/v1/donations/currentuser?skip={skip}&top={top}&orderby={orderby}&includeCount={includeCount}";
+         $rootScope.donatieLijst            = api + "v1/donations/currentuser?skip={skip}&top={top}&orderby={orderby}&includeCount={includeCount}";
          $rootScope.jaarOpgave              = api + "v1/donations/report/annualstatement/{year}";
 
          // Goede doelen
